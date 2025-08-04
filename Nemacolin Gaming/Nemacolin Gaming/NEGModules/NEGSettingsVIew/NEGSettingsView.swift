d10937c1c4d109b343e2026dd06e832f960aa616
Dias <dias.atudinov@gmail.com>
2025-08-04 19:02:38 +0500

    new files

---
+++ Nemacolin Gaming/Nemacolin Gaming/NEGModules/NEGSettingsVIew/NEGSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  NEGSettingsView.swift
+//  Nemacolin Gaming
+//
+//  Created by Dias Atudinov on 04.08.2025.
+//
+
+import SwiftUI
+
+struct NEGSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NEGSettingsView()
+}
