0f9acfb3e91f38aae622853c8c2d10cbd0af34ce
Dias <dias.atudinov@gmail.com>
2025-08-04 18:21:12 +0500

    app setup

---
+++ Nemacolin Gaming/Nemacolin Gaming/Nemacolin_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nemacolin_GamingApp.swift
+//  Nemacolin Gaming
+//
+//  Created by Dias Atudinov on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Nemacolin_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
