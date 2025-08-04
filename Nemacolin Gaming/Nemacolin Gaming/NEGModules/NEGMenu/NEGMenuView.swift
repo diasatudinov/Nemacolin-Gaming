88477d17caf60a8a1781eba1f7a1ec34f5c2b995
Dias <dias.atudinov@gmail.com>
2025-08-04 18:47:38 +0500

    add menu view

--- Nemacolin Gaming/Nemacolin Gaming/NEGModules/NEGMenu/NEGMenuView.swift
+++ Nemacolin Gaming/Nemacolin Gaming/NEGModules/NEGMenu/NEGMenuView.swift
@@ -2,15 +2,113 @@
 //  NEGMenuView.swift
 //  Nemacolin Gaming
 //
-//  Created by Dias Atudinov on 04.08.2025.
 //
 
 import SwiftUI
 
 struct NEGMenuView: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+    @State private var showDailyTask = false
+    
+//    @StateObject var achievementVM = NGAchievementsViewModel()
+//    @StateObject var settingsVM = NGSettingsViewModel()
+//    @StateObject var shopVM = NGShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            VStack(spacing: 0) {
+                HStack(alignment: .center, spacing: 20) {
+                    
+                    Spacer()
+                    
+                    NGCoinBg()
+                    
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconNEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NGDeviceManager.shared.deviceType == .pad ? 100:50)
+                    }
+                    
+                }.padding(15)
+                
+                Spacer()
+                VStack(spacing: 7) {
+                    HStack(alignment: .top) {
+                        Button {
+                            showGame = true
+                        } label: {
+                            Image(.playIconNEG)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        Button {
+                            showDailyTask = true
+                        } label: {
+                            Image(.dailyTasksIconNEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NGDeviceManager.shared.deviceType == .pad ? 120:66)
+                        }.offset(y: -5)
+                        
+                    }.frame(height: NGDeviceManager.shared.deviceType == .pad ? 160:88)
+                    
+                    Button {
+                        showShop = true
+                    } label: {
+                        Image(.shopIconNEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NGDeviceManager.shared.deviceType == .pad ? 160:88)
+                    }.offset(x: 20)
+                    
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        Image(.achievementsIconNEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NGDeviceManager.shared.deviceType == .pad ? 160:88)
+                    }.offset(x: -30)
+                }
+                Spacer()
+            }
+        }
+        .background(
+            ZStack {
+                Image(.appBgNEG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+        .fullScreenCover(isPresented: $showGame) {
+//            NGRoundSelectionView()
+        }
+        .fullScreenCover(isPresented: $showAchievement) {
+//            NGAchievementsView(viewModel: achievementVM)
+        }
+        .fullScreenCover(isPresented: $showShop) {
+//            NGShopView(viewModel: shopVM)
+        }
+        .fullScreenCover(isPresented: $showSettings) {
+//            NGSettingsView(settingsVM: settingsVM)
+        }
+        .fullScreenCover(isPresented: $showDailyTask) {
+//            NGDailyTasksView()
+        }
     }
+    
 }
 
 #Preview {
