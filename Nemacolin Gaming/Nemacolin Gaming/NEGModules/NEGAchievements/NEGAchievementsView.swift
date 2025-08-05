c503307ed544f9a687535af52212f9cb5c1448b4
Dias <dias.atudinov@gmail.com>
2025-08-05 20:24:38 +0500

    new files add

---
+++ Nemacolin Gaming/Nemacolin Gaming/NEGModules/NEGAchievements/NEGAchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  NEGAchievementsView.swift
+//  Nemacolin Gaming
+//
+//  Created by Dias Atudinov on 05.08.2025.
+//
+
+import SwiftUI
+
+struct NEGAchievementsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NEGAchievementsView()
+}
